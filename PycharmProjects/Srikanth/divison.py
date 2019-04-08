d9c45a2eb9f57bc3dc8cd4dc3e17cf2745f8f978
Srikanth Garnepalli <srikanthgarnepalli@Srikanths-MacBook-Air.local>
2019-04-08 13:51:54 +0530

    division

---
+++ PycharmProjects/Srikanth/divison.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from __future__ import division
+def main():
+    print a // b
+    print a/b
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    main()
+
+
